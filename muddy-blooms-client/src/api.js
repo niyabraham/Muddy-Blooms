e79d569d0d0ed92567424c414c409d4c6c3c9621
niyabraham <niyaabraham06@gmail.com>
2026-06-18 02:27:27 +0530

    Connect frontend to live backend on Render

--- muddy-blooms-client/src/api.js
+++ muddy-blooms-client/src/api.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:5000/api';
+const API = 'https://muddy-blooms-api.onrender.com/api';
 
 export const fetchPlants = async () => {
   const res = await fetch(`${API}/plants`);
